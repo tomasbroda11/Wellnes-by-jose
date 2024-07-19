c3b4416bb0357c74f83b3ae0e18857af329ee314
Tomas Broda <tomasbroda13@gmail.com>
2024-07-19 17:26:52 -0300

    pequeños cambios

--- create_admin.py
+++ create_admin.py
@@ -15,21 +15,6 @@ def create_admin():
         db.session.commit()
         print("Admin user created successfully.")
 
-""" def hash_existing_passwords():
-    with app.app_context():
-        # Obtener todos los usuarios de la base de datos
-        users = User.query.all()
-
-        # Iterar sobre cada usuario y hashear su contraseña
-        for user in users:
-            # Hashear la contraseña si no está ya hasheada (para evitar rehashear)
-            if not user.password.startswith('$2b$'):
-                hashed_password = generate_password_hash(user.password)
-                user.password = hashed_password
-
-        # Confirmar los cambios en la base de datos
-        db.session.commit()
-        print("Contraseñas hasheadas correctamente.") """
 
 if __name__ == '__main__':
     create_admin()
